03006693af5b4ffe5b16d6e4ad5b32cc68988e25
Karthika M <qnkarthika@gmail.com>
2025-01-21 23:29:46 +0530

    fourth commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-@87be)q^1(a-*m49d*!@-t4=2adwvqt1_=^v$p0ir4p6rhxu7@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'django-chat-application-1.onrender.com',
+    '127.0.0.1', 
+]
+
 
 
 # Application definition
